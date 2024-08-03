b6a7bd2797fb5b7edde09efb61f2a5e54dfddce0
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-08-03 02:06:03 -0700

    Make project dark mode for shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
+import { ThemeProvider } from '@/components/ui/theme-provider'
 
 import { cn } from '../lib/utils'
 
@@ -23,7 +24,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
-        {children}
+        <ThemeProvider attribute='class' defaultTheme='dark'>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
