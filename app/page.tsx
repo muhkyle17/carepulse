52ecb078c8ae481a6dc57ddb79c114211afde993
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-08-07 16:24:49 -0700

    Create the patient form page with footer and logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button'
+import Link from 'next/link'
 import Image from 'next/image'
 
 import PatientForm from '@/components/forms/PatientForm'
@@ -17,8 +18,23 @@ export default function Home() {
           />
 
           <PatientForm />
+
+          <div className='text-14-regular mt-20 flex justify-between'>
+            <p className='justify-items-end text-dark-600 xl:text-left'>© 2024 CarePulse</p>
+            <Link href='/?admin=true' className='text-green-500'>
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+
+      <Image
+        src='/assets/images/onboarding-img.png'
+        height={1000}
+        width={1000}
+        alt='patient'
+        className='side-img max-w-[50%]'
+      />
     </div>
   )
 }
