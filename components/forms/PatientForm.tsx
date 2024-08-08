4b3d49615ee3d8f045b8c547de9c07f82a2cf3dd
Mikyle Reyes <reyesmikyle17@gmail.com>
2024-08-07 17:20:47 -0700

    Create a separate component for custom form field

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -5,16 +5,8 @@ import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form'
-import { Input } from '@/components/ui/input'
+import { Form } from '@/components/ui/form'
+import CustomFormField from '@/components/CustomFormField'
 
 const formSchema = z.object({
   username: z.string().min(2, {
@@ -42,20 +34,8 @@ const PatientForm = () => {
           <p className='text-dark-700'>Schedule your first appointment</p>
         </section>
 
-        <FormField
-          control={form.control}
-          name='username'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder='shadcn' {...field} />
-              </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
+        <CustomFormField control={form.control} />
+
         <Button type='submit'>Submit</Button>
       </form>
     </Form>
